62ee3306f74e979b062d55f82a1f68765b9e470d
Enge-Olate <marcioalexisolate@live.com>
2025-11-18 10:35:20 -0300

    Adicionando teste negativo ao input '#telefone' quando vazio.

--- cypress/e2e/conviteNiver.cy.js/checkout.cy.js
+++ cypress/e2e/conviteNiver.cy.js/checkout.cy.js
@@ -1,5 +1,6 @@
 ///<reference types="cypress" />
 
+
 describe('Deve carregar à página e verificar se tem título', ()=>{
     beforeEach(()=>{
         cy.visit('http://convite-aniversario-eight.vercel.app/')
@@ -10,17 +11,23 @@ describe('Deve carregar à página e verificar se tem título', ()=>{
     it('Deve abrir o formulário', ()=>{
         cy.get('#button-form').click()
         cy.get('#nome').type('usuario_teste')
-       cy.get('#telefone').type(35991530033)
-       cy.get('#presenca').select('Não')
-       cy.get('#botao-confirmacao').click()
+        cy.get('#telefone').type(35991530033)
+        cy.get('#presenca').select('Não')
+        cy.get('#botao-confirmacao').click()
 
     })
 
-    // it('Deve permitir fazer a confirmação de comparecimento ao evento.',()=>{
-    //    cy.get('#nome').type('usuario_teste')
-    //    cy.get('#telefone').type(35991530033)
-    //    cy.get('#presenca').type('sim') 
-    //    cy.get('#botao-confirmacao').click()
+    it('Deve retornar erro com os input telefone vazio', ()=>{
+        cy.get('#button-form').click()
+        cy.get('#nome').type('usuario_teste')
+        // cy.get('#telefone').type(35991530033)
+        cy.get('#presenca').select('Não')
+        cy.on('window:alert', (msg)=>{
+                expect(msg).to.contain('Por favor, preencha todos os campos antes de confirmar sua presença.')
+            })
 
-    // })
+        cy.get('#botao-confirmacao').click()
+    })
+    
 })
+        
